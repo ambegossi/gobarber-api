5119b48230f724e3a62b206f3cbad5ca405f9106
ambegossi <andersonbegossi@gmail.com>
2020-02-04 16:47:15 -0300

    Add Files Upload feature

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -25,6 +25,10 @@ class User extends Model {
     return this; // returns the model that has just been created
   }
 
+  static associate(models) {
+    this.belongsTo(models.File, { foreignKey: 'avatar_id' });
+  }
+
   checkPassword(password) {
     return bcrypt.compare(password, this.password_hash); // async
   }
